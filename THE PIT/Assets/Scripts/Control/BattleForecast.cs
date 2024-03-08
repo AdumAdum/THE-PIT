1922e8da528a953cb3aec5d0d567130aac917767
AdumAdumAdum <adamsellers2020@gmail.com>
2024-03-08 00:08:15 -0800

    Battle Forecast UI done

---
+++ THE PIT/Assets/Scripts/Control/BattleForecast.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BattleForecast : MonoBehaviour
+{
+    [SerializeField] List<Image> pIcons;
+    [SerializeField] List<CUIText> pInfo;
+    [SerializeField] List<CUIText> pTextFields;
+    [SerializeField] List<CUIText> pIntFields;
+
+    [SerializeField] List<Image> eIcons;
+    [SerializeField] List<CUIText> eInfo;
+    [SerializeField] List<CUIText> eTextFields;
+    [SerializeField] List<CUIText> eIntFields;
+
+    
+
+}
